377a1fae5b90c4aff7b5576943682e361ae80172
eon <noe.navarro1019@gmail.com>
2022-01-03 23:34:12 -0700

    created test for stats and inventory on Player.js

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -14,5 +14,23 @@ test('creates a player object', () => {
     );
 })
 
+test("gets player's stats as a n object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+})
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));  
+    player.inventory = [];
+    expect(player.getInventory()).toEqual(false);
+  });
+
 jest.mock('../lib/Potion');
 console.log(new Potion());
+
